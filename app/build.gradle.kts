11f4a4eca5a5358019339edee03136a01f69705e
IAmOkey <artem.to2018@yandex.ru>
2025-11-02 18:36:07 +0300

    Птыаюсь починить

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:test-manifest")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // --- Navigation Compose ---
     implementation("androidx.navigation:navigation-compose:2.8.3")
