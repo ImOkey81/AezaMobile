e294c2f91258cdce0437fb3146ba05c897bbe82d
ImOkey81 <artem.to2018@icloud.com>
2025-11-08 11:36:24 +0300

    Send check type enum values in uppercase

--- app/src/main/java/aeza/hostmaster/mobile/data/repository/CheckRepository.kt
+++ app/src/main/java/aeza/hostmaster/mobile/data/repository/CheckRepository.kt
@@ -2,13 +2,16 @@ package aeza.hostmaster.mobile.data.repository
 
 import aeza.hostmaster.mobile.data.model.CheckRequestDto
 import aeza.hostmaster.mobile.data.remote.ApiService
+import java.util.Locale
 import javax.inject.Inject
 
 class CheckRepository @Inject constructor(
     private val api: ApiService
 ) {
-    suspend fun submitCheck(target: String, type: String) =
-        api.submitCheck(CheckRequestDto(target, type))
+    suspend fun submitCheck(target: String, type: String) {
+        val normalizedType = type.uppercase(Locale.ROOT)
+        api.submitCheck(CheckRequestDto(target, normalizedType, normalizedType))
+    }
 
     suspend fun getStatus(jobId: String) = api.getCheckStatus(jobId)
 }
