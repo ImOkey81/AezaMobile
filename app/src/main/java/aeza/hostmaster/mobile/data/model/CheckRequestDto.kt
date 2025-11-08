8f98479fb62a0ad9230e34ecdf79cdbd46935f4e
ImOkey81 <artem.to2018@icloud.com>
2025-11-08 11:47:08 +0300

    Align check payload with backend field naming

--- app/src/main/java/aeza/hostmaster/mobile/data/model/CheckRequestDto.kt
+++ app/src/main/java/aeza/hostmaster/mobile/data/model/CheckRequestDto.kt
@@ -1,6 +1,10 @@
 package aeza.hostmaster.mobile.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CheckRequestDto(
     val target: String,
-    val type: String
+    val type: String,
+    @SerializedName(value = "checkType", alternate = ["check_type"])
+    val checkType: String
 )
--- app/src/main/java/aeza/hostmaster/mobile/data/repository/CheckRepository.kt
+++ app/src/main/java/aeza/hostmaster/mobile/data/repository/CheckRepository.kt
@@ -2,13 +2,20 @@ package aeza.hostmaster.mobile.data.repository
 
 import aeza.hostmaster.mobile.data.model.CheckRequestDto
 import aeza.hostmaster.mobile.data.remote.ApiService
+import java.util.Locale
 import javax.inject.Inject
 
 class CheckRepository @Inject constructor(
     private val api: ApiService
 ) {
-    suspend fun submitCheck(target: String, type: String) =
-        api.submitCheck(CheckRequestDto(target, type))
+    suspend fun submitCheck(target: String, type: String) {
+        val request = CheckRequestDto(
+            target = target,
+            type = type,
+            checkType = type.uppercase(Locale.ROOT)
+        )
+        api.submitCheck(request)
+    }
 
     suspend fun getStatus(jobId: String) = api.getCheckStatus(jobId)
 }
