d7847cdd1022901189be521d5bb61a26bc784aae
ImOkey81 <artem.to2018@icloud.com>
2025-11-23 19:27:06 +0300

    Poll check results via job result endpoint

--- app/src/main/java/aeza/hostmaster/mobile/data/remote/ApiService.kt
+++ app/src/main/java/aeza/hostmaster/mobile/data/remote/ApiService.kt
@@ -9,6 +9,6 @@ interface ApiService {
     @POST("checks")
     suspend fun submitCheck(@Body request: CheckRequestDto): CheckResponseDto
 
-    @GET("checks/{checkId}/result")
-    suspend fun getCheckStatus(@Path("checkId") checkId: String): CheckResponseDto
+    @GET("checks/{jobId}/result")
+    suspend fun getCheckResult(@Path("jobId") jobId: String): CheckResponseDto
 }
