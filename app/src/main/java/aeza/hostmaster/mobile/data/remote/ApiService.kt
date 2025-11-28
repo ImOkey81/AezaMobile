cc88010134c5bfe611e4dc06fa24b4711f5c5dfe
ImOkey81 <artem.to2018@icloud.com>
2025-11-28 21:33:27 +0300

    Add headers to ensure JSON check-host responses

--- app/src/main/java/aeza/hostmaster/mobile/data/remote/ApiService.kt
+++ app/src/main/java/aeza/hostmaster/mobile/data/remote/ApiService.kt
@@ -3,23 +3,29 @@ package aeza.hostmaster.mobile.data.remote
 import aeza.hostmaster.mobile.data.model.CheckHostResultDto
 import aeza.hostmaster.mobile.data.model.CheckHostStartResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
 
+    @Headers("Accept: application/json")
     @GET("check-ping")
     suspend fun startPing(@Query("host") host: String): CheckHostStartResponseDto
 
+    @Headers("Accept: application/json")
     @GET("check-http")
     suspend fun startHttp(@Query("host") host: String): CheckHostStartResponseDto
 
+    @Headers("Accept: application/json")
     @GET("check-tcp")
     suspend fun startTcp(@Query("host") host: String): CheckHostStartResponseDto
 
+    @Headers("Accept: application/json")
     @GET("check-dns")
     suspend fun startDns(@Query("host") host: String): CheckHostStartResponseDto
 
-    @GET("check-result/{requestId}")
+    @Headers("Accept: application/json")
+    @GET("check-result-extended/{requestId}")
     suspend fun fetchResult(@Path("requestId") requestId: String): CheckHostResultDto
 }
